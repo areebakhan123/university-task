df4fd9b1389e4d48a6be5e09aec674c607408f2c
areebakhan123 <49521463+areebakhan123@users.noreply.github.com>
2019-09-06 21:27:36 +0500

    upload files

---
+++ tk-june-22-master/images/script.js
@@ -0,0 +1,32 @@
+let busSlide = document.querySelector("#bus-image")
+let appleSlide = document.querySelector("#apple-image")
+let breadSlide = document.querySelector("#bread-image")
+
+
+function currentSlide1() {
+   busSlide.style.display = 'block';
+   appleSlide.style.display = 'none';
+   breadSlide.style.display = 'none';
+}
+
+function currentSlide2() {
+   appleSlide.style.display = 'block';
+   breadSlide.style.display = 'none';
+   busSlide.style.display = 'none';
+}
+
+function currentSlide3() {
+   breadSlide.style.display = 'block';
+    appleSlide.style.display = 'none';
+   busSlide.style.display = 'none';
+}
+
+// function show() {
+//    if (document.querySelector("#hamburger").style.display = "block") {
+//       document.querySelector("#hamburger li").style.display = "none";
+//    }else {
+//       document.querySelector("#hamburger").style.display = "none"
+//       document.querySelector("#hamburger li").style.display = "block";
+
+//    }
+// }
